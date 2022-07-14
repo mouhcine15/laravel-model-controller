5933da0c0231f0461ba89b9fa142a955e27c6d23
mouhcine <mouhcine15.00@gmail.com>
2022-07-14 18:15:31 +0200

    esercizio completo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,18 +8,23 @@
             @foreach ($movie as $movies)
                 <li>
                     <div>
+                        <strong>Titolo: </strong>
                         {{$movies->title}}
                     </div>
                     <div>
+                        <strong>Titolo originale: </strong>
                         {{$movies->original_title}}
                     </div>
                     <div>
+                        <strong>Nazionalità: </strong>
                         {{$movies->nationality}}
                     </div>
                     <div>
+                        <strong>Data di uscita: </strong>
                         {{$movies->date}}
                     </div>
                     <div>
+                        <strong>Voto: </strong>
                         {{$movies->vote}}
                     </div>
                 </li>   
